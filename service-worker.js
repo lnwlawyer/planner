3c6a9b3493bf199bf31c00c4c46708b76a653b39
lnwlawyer <boonprasertlawyer@gmail.com>
2026-01-14 16:39:53 +0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+// Basic Service Worker to enable PWA installability
+const CACHE_NAME = 'omniplan-v1';
+const urlsToCache = [
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
